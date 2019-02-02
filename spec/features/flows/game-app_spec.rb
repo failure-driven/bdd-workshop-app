5810b82485a5c61f2e694ac6ec06490ced09aaa8
Michael Milewski <saramic@gmail.com>
2019-02-03 10:13:35 +1100

    quick and nasty way to check for ruby/rails version

--- spec/features/flows/game-app_spec.rb
+++ spec/features/flows/game-app_spec.rb
@@ -9,6 +9,13 @@ feature 'Game App', js: true do
     Then 'user sees they are on rails' do
       # note the apostrophe next line
       wait_for { page }.to have_content("Yay! You’re on Rails!")
+      version = page.find('p.version')
+      wait_for {
+        version.text[/(Rails version: )(?<version>[^\n]*)/, 'version']
+      }.to match(/^6.0.0/)
+      wait_for {
+        version.text[/(Ruby version: )(?<version>[^\n]*)/, 'version']
+      }.to match(/^ruby 2.6.0/)
     end
   end
 end

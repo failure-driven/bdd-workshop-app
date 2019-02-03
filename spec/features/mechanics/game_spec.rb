1a0f8ab268f3dc6ffc2f014bcfd26413c4f355aa
Michael Milewski <saramic@gmail.com>
2019-02-03 11:27:20 +1100

    :construction: add controller to hold our react SPA

--- spec/features/mechanics/game_spec.rb
+++ spec/features/mechanics/game_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 feature 'Game', js: true do
   scenario 'game is a React single page app (SPA)' do
     When 'user visits the game' do
-      pending("we don't have route")
       visit('/game')
     end
 
     Then 'user sees they are on react' do
-      wait_for { focus_on(:game).message_and_versions }.to eq(
-        message: 'You are on react',
-        react:   '16.7',
-        node:    '11.11.0'
+      pending("We're not rendering a ReactJS component")
+      wait_for { focus_on(:game).message_and_versions }.to include(
+        message:       'You are on React',
+        react_version: match(/16\.7/)
       )
     end
   end
--- spec/support/features/page_fragments/game.rb
+++ spec/support/features/page_fragments/game.rb
@@ -2,7 +2,7 @@ module PageFragments
   module Game
     def message_and_versions
       {
-        message: browser.find('div[@data-test="message"]').text,
+        message: browser.find('div[data-test="message"]').text,
         react_version: browser.find('span[data-test="react-version"]').text,
       }
     end

f6ef499c6ba3bd41c78c19a318b2e8473f8d91c5
Michael Milewski <saramic@gmail.com>
2019-02-03 21:57:12 +1100

    :tada: a first component tested with enzyme

---
+++ app/javascript/packs/game.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Game = props => <div>{React.version}</div>;
+
+export default Game;
--- app/javascript/packs/game.test.js
+++ app/javascript/packs/game.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Game from './game';
+import Adapter from 'enzyme-adapter-react-16';
 
-describe('game', () => {
-  it('we have jest tests', () => {
-    expect(true).toEqual(true);
+configure({ adapter: new Adapter() });
+
+describe('Game', () => {
+  it('renders a <div>', () => {
+    const wrapper = shallow(<Game />);
+    expect(wrapper.type()).toBe('div');
+    expect(wrapper.prop('children')).toBe('16.7.0');
   });
 });

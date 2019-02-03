036a3ad22e2f0dc3934803faf4d4b68a11e26bd8
Michael Milewski <saramic@gmail.com>
2019-02-03 17:53:38 +1100

    :wrench: jest test setup

---
+++ app/javascript/packs/game.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+describe('game', () => {
+  it('we have jest tests', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  testPathIgnorePatterns: ['config/webpack', 'node_modules']
+};

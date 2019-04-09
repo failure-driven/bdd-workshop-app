7f7dd445a4954dd10dc73fa830496eb40a7abc66
Michael Milewski <saramic@gmail.com>
2019-04-10 00:15:11 +1000

    first go at monster files

---
+++ spec/features/monster_no_fragments_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+feature 'Signup to play the game', js: true do
+  scenario 'A couple of users signup and they need unique "handles"' do
+    visit('/')
+    page.find(".jumbotron[data-testid='wargames']").click
+    wait_for do
+      page.find("main[data-testid='register'] h1").text
+    end.to eq('Please sign in or create a profile!')
+    page.find("main[data-testid='register']").fill_in('handle', with: 'princess')
+    page.fill_in('handle', with: 'princess')
+    page.find('[name="submit"]').click
+    page.fill_in('email', with: 'princess@email.com')
+    page.find('[name="submit"]').click
+    wait_for { page.find('.alert.alert-info').text }.to eq('Updated user profile')
+    wait_for { page.find("nav .nav-link[data-testid='profile-link']").text }.to eq('princess')
+    wait_for { page.find("main[data-testid='game'] h1").text }.to eq('coming soon')
+    # fix with user signs out
+    page.execute_script("window.localStorage.setItem('player','{}')")
+    visit('/')
+    page.find(".jumbotron[data-testid='wargames']").click
+    wait_for do
+      page.find("main[data-testid='register'] h1").text
+    end.to eq('Please sign in or create a profile!')
+    page.fill_in('handle', with: 'troll')
+    page.find('[name="submit"]').click
+    page.fill_in('email', with: 'troll@email.com')
+    page.find('[name="submit"]').click
+    wait_for { page.find('.alert.alert-info').text }.to eq('Updated user profile')
+    wait_for { page.find("nav .nav-link[data-testid='profile-link']").text }.to eq('troll')
+    wait_for { page.find("main[data-testid='game'] h1").text }.to eq('coming soon')
+    # fix with user signs out
+    page.execute_script("window.localStorage.setItem('player','{}')")
+    visit('/')
+    page.find('nav .nav-link', text: 'Sign in').click
+    page.fill_in('handle', with: 'princess')
+    page.find('[name="submit"]').click
+    wait_for { page.find('.alert.alert-info').text }.to eq('signed in successfully')
+    wait_for { page.find("nav .nav-link[data-testid='profile-link']").text }.to eq('princess')
+    wait_for { page.find_all('nav .nav-link').map(&:text) }.to eq ['princess']
+    page.find('nav .nav-link', text: 'princess').click
+    page.find("main[data-testid='profile']").click_on('Edit')
+    page.fill_in('handle', with: 'troll')
+    page.find('[name="submit"]').click
+    wait_for { page.find('.alert.alert-danger').text }.to eq('handle: has already been taken')
+    wait_for { page.find('[name="handle"]').value }.to eq('troll')
+    page.refresh
+    wait_for { page.find('[data-testid="details-handle"]').text }.to eq('princess')
+    page.find("main[data-testid='profile']").click_on('Edit')
+    page.fill_in('handle', with: 'disney_princess')
+    page.find('[name="submit"]').click
+    wait_for { page.find('.alert.alert-info').text }.to eq('Updated user profile')
+  end
+end
---
+++ spec/features/monster_spec_no_when_then.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+feature 'Signup to play the game', js: true do
+  scenario 'A couple of users signup and they need unique "handles"' do
+    visit('/')
+    focus_on(:game_actions).for_game('wargames').play
+    wait_for do
+      focus_on(:page_content).container_for('register').heading
+    end.to eq('Please sign in or create a profile!')
+    focus_on(:form).form_for('register').fill_in_row_for('handle', 'princess')
+    focus_on(:form).form_for('register').submit
+    focus_on(:form).form_for('profile').fill_in_row_for('email', 'princess@email.com')
+    focus_on(:form).form_for('profile').submit
+    wait_for { focus_on(:messages).info }.to eq('Updated user profile')
+    wait_for { focus_on(:nav).nav_link('profile') }.to eq('princess')
+    wait_for { focus_on(:page_content).container_for('game').heading }.to eq('coming soon')
+    # fix with user signs out
+    page.execute_script("window.localStorage.setItem('player','{}')")
+    visit('/')
+    focus_on(:game_actions).for_game('wargames').play
+    wait_for do
+      focus_on(:page_content).container_for('register').heading
+    end.to eq('Please sign in or create a profile!')
+    focus_on(:form).form_for('register').fill_in_row_for('handle', 'troll')
+    focus_on(:form).form_for('register').submit
+    focus_on(:form).form_for('profile').fill_in_row_for('email', 'troll@email.com')
+    focus_on(:form).form_for('profile').submit
+    wait_for { focus_on(:messages).info }.to eq('Updated user profile')
+    wait_for { focus_on(:nav).nav_link('profile') }.to eq('troll')
+    wait_for { focus_on(:page_content).container_for('game').heading }.to eq('coming soon')
+    # fix with user signs out
+    page.execute_script("window.localStorage.setItem('player','{}')")
+    visit('/')
+    focus_on(:nav).follow_nav_link('Sign in')
+    focus_on(:form).form_for('sign-in').fill_in_row_for('handle', 'princess')
+    focus_on(:form).form_for('sign-in').submit
+    wait_for { focus_on(:messages).info }.to eq('signed in successfully')
+    wait_for { focus_on(:nav).nav_link('profile') }.to eq('princess')
+    wait_for { focus_on(:nav).nav_links }.to eq ['princess']
+    focus_on(:nav).follow_nav_link('princess')
+    focus_on(:page_content).container_for('profile').action_item('Edit')
+    focus_on(:form).form_for('profile').fill_in_row_for('handle', 'troll')
+    focus_on(:form).form_for('profile').submit
+    wait_for { focus_on(:messages).error }.to eq('handle: has already been taken')
+    wait_for { focus_on(:form).form_for('profile').field('handle') }.to eq('troll')
+    page.refresh
+    wait_for { focus_on(:profile).details[:handle] }.to eq('princess')
+    focus_on(:page_content).container_for('profile').action_item('Edit')
+    focus_on(:form).form_for('profile').fill_in_row_for('handle', 'disney_princess')
+    focus_on(:form).form_for('profile').submit
+    wait_for { focus_on(:messages).info }.to eq('Updated user profile')
+  end
+end
